e5263a0d14b69bb7c560c0f7aa73f19ba845cedd
juanrg <juanramon.gonzalezvallinas@upf.edu>
2013-05-16 16:47:55 +0200

    reverse strand support

--- dna_from_ucsc.py
+++ dna_from_ucsc.py
@@ -11,6 +11,27 @@ url = "http://genome.ucsc.edu/cgi-bin/das/hg19/dna?segment=%s:%s,%s"
 for line in open(region_path):
     sline = line.split()
     sequence = urllib.urlopen(url%(sline[0], sline[1], sline[2])).read().split("<DNA")[1].split(">")[1].split("<")[0].replace("\n", "")
+
+    #reverse the sequence if the strand is reverse
+    sequence = sequence.lower()
+    if len(sline) > 5:
+        if sline[5] == "-":
+            new_seq = []
+            for char in sequence[::-1]:
+                if char == 'g':
+                    new_seq.append('c')
+                elif char == 'c':
+                    new_seq.append('g')
+                elif char == 'a':              
+                    new_seq.append('t')      
+                elif char == 't':              
+                    new_seq.append('a')             
+                else:
+                    new_seq.append(char)
+
+            sequence = ''.join(new_seq)
+
+
     print "%s\t%s"%(line.strip("\n"), sequence)
 
 
